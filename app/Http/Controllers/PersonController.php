15ece9cf393279ec51d546bf44e7cbf725cddcce
Richard Sengkey <richardsimonb@gmail.com>
2026-04-24 07:12:36 +0700

    updated validation logic and add exportCsv logic

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Person;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
 
 class PersonController extends Controller
 {
@@ -37,10 +38,14 @@ class PersonController extends Controller
         if(!$request->isJson()) {
             return response()->json(['message' => 'Only Accept JSON'], 400);
         }
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string',
-            'phone_number' => 'required|string',
+            'phone_number' => 'required|string|regex:/^[0-9]{7,12}$/',
         ]);
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Validation failed', 'errors' => $validator->errors()], 422);
+        }
+
         if (Person::where('name', $request->name)->exists()) {
             return response()->json(['message' => 'Data already exists'], 409);
         }
@@ -51,18 +56,25 @@ class PersonController extends Controller
         ], 200);
     }
 
-    public function update(Request $request, $id) : JsonResponse
+    public function update(Request $request) : JsonResponse
     {
-        $person = Person::find($id);
-        if (!$person) {
-            return response()->json(['message' => 'Data not found'], 404);
-        }
         if(!$request->isJson()) {
             return response()->json(['message' => 'Only Accept JSON'], 400);
         }
-        $request->validate([
-            'phone_number' => 'required|string',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'phone_number' => 'required|string|regex:/^[0-9]{7,12}$/',
         ]);
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Validation failed', 'errors' => $validator->errors()], 422);
+        }
+        $person = Person::where('name', $request->name)->first();
+        if (!$person) {
+            return response()->json(['message' => 'Data not found'], 404);
+        }
+        if ($person->phone_number === $request->phone_number) {
+            return response()->json(null, 204);
+        }
         $person->phone_number = $request->phone_number;
         $person->update();
         return response()->json([
@@ -80,5 +92,22 @@ class PersonController extends Controller
         $person->delete();
         return response()->json(['message' => 'Data deleted successfully'], 200);
     }
+
+    public function exportCsv() {
+    $callback = function() {
+        $file = fopen('php://output', 'w');
+        fputcsv($file, ['Name', 'Phone Number']);
+        
+        Person::query()->each(function($person) use($file) {
+            fputcsv($file, [$person->name, $person->phone_number]);
+        });
+        fclose($file);
+    };
+
+    return response()->stream($callback, 200, [
+        "Content-type" => "text/csv",
+        "Content-Disposition" => "attachment; filename=person.csv",
+    ]);
+}
        
 }
