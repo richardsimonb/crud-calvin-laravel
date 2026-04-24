389c6746b5d823f8c69dad2338b69abfeb603f14
Richard Sengkey <richardsimonb@gmail.com>
2026-04-24 07:12:53 +0700

    created layout file

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,10 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @viteReactRefresh
+            @vite(['resources/css/app.css', 'resources/js/main.tsx'])
+        @endif
         <title>{{ config('app.name', 'Laravel') }}</title>
     </head>
-    <body>
+    <body class="flex flex-col min-h-screen max-w-7xl mx-auto items-center p-4">
         {{ $slot }}
     </body>
 </html>
